758e4558478663304e23507602b19976719b640e
Piotr Skowronek <p.skowronek90@gmail.com>
2018-03-26 22:14:42 +0200

    fix

--- src/AppBundle/Controller/RegisterController.php
+++ src/AppBundle/Controller/RegisterController.php
@@ -4,8 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
 
 class RegisterController extends Controller
 {
@@ -18,25 +18,33 @@ class RegisterController extends Controller
     }
 
     /**
-     * @Route("/register", name="register-post" methods={"POST"})
+     * @Route("/register", name="register-post", methods={"POST"})
      */
     public function registerVerifyAction(Request $request)
     {
+        $username = $request->get('username');
         $email = $request->get('email');
         $password = $request->get('password');
-        $confirm = $request->get('password_confirm');
-        $agree = $request->get('agree');
+        $hashPassword = password_hash($password, PASSWORD_DEFAULT);
+
         $filter = filter_var($email, FILTER_VALIDATE_EMAIL);
 
         if ($filter == false) {
-            return new Response("Incorrect data");
-        } elseif ($password != $confirm) {
-            return new Response("Passwords must be the same");
-        } elseif ($agree != true) {
-            return new Response("You must acept the rules");
+            return $this->render("register/created-user.html.twig");
         } else {
-            return new Response("User registered");
+
+            $user = new User();
+            $user->setUsername($username);
+            $user->setEmail($email);
+            $user->setPassword($hashPassword);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->render("register/created-user.html.twig", ['username' => $user->getUsername()]);
         }
 
     }
+
 }
