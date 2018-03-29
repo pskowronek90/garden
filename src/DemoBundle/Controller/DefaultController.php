869f49501f3a45fd736d7960b3cb1a2b8ad0cc05
Piotr Skowronek <p.skowronek90@gmail.com>
2018-03-29 20:21:39 +0200

    working logging-in

--- src/DemoBundle/Controller/DefaultController.php
+++ src/DemoBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace DemoBundle\Controller;
 
+use AppBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -14,4 +17,28 @@ class DefaultController extends Controller
     {
         return $this->render('demo/demo.html.twig');
     }
+
+    /**
+     * @Route("/homepage", name="panel-login", methods={"POST"})
+     */
+    public function loginAction(Request $request)
+    {
+        $login = $request->get('login-panel');
+        $password = $request->get('password-panel');
+
+        $passVerify = password_verify($password, PASSWORD_DEFAULT);
+
+        $em = $this->getDoctrine()->getManager();
+        $userRepository = $em->getRepository(User::class);
+
+        $validLogin = $userRepository->findOneBy(['login' => $login, 'password' => $passVerify]);
+
+        if (!$validLogin) {
+            throw new NotFoundHttpException();
+        } else {
+            $this->render("demo/demo.html.twig");
+        }
+
+    }
+
 }
