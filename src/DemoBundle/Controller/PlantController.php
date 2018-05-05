7f5b09ca6b1debc01359cad4b4e957ad83a53a62
Piotr Skowronek <p.skowronek90@gmail.com>
2018-05-05 15:56:13 +0200

    admin panel in progress

--- src/DemoBundle/Controller/PlantController.php
+++ src/DemoBundle/Controller/PlantController.php
@@ -5,6 +5,33 @@ namespace DemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/plant")
+ */
+
 class PlantController extends Controller
 {
+    /**
+     * @Route("/new", name="plant-new-get", methods={"GET"})
+     */
+    public function newGetAction()
+    {
+        return $this->render("admin/plant/new.html.twig");
+    }
+
+    /**
+     * @Route("/edit", name="plant-edit-get", methods={"GET"})
+     */
+    public function editGetAction()
+    {
+        return $this->render("admin/plant/edit.html.twig");
+    }
+
+    /**
+     * @Route("/delete", name="plant-delete-get", methods={"GET"})
+     */
+    public function deleteGetAction()
+    {
+        return $this->render("admin/plant/delete.html.twig");
+    }
 }
